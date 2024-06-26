d77d1a52beb5378fd33661ac534842f94b34f335
suhasledange <suhasledange10@gmail.com>
2024-06-26 14:54:01 +0530

    menu fix

--- src/app/components/sales-log-table/sales-log-table.component.ts
+++ src/app/components/sales-log-table/sales-log-table.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit,Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatSort, MatSortModule } from '@angular/material/sort';
